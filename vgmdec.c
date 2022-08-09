e743af5efb46832d9736390d6cced049f5873ae4
baoshi <baoshi.zhu@gmail.com>
2022-08-09 18:51:40 +0800

    show cache status

--- vgmdec.c
+++ vgmdec.c
@@ -11,8 +11,19 @@ int main(int argc, char *argv[])
     }
 
     file_reader_t *reader = cfr_create(argv[1], 4096);
+    if (!reader)
+    {
+        VGM_PRINTERR("Unable to open %s\n", argv[1]);
+        return -1;
+    }
+        
     vgm_t *vgm = vgm_create(reader);
-
+    if (vgm)
+    {
+        vgm_destroy(vgm);
+    }
+    
+    cfr_show_cache_status(reader);
     cfr_destroy(reader);
 
     return 0;
