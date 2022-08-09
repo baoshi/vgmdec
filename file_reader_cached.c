34b399a4d6e33ac393b33c0cadc7056ee8fedd82
baoshi <baoshi.zhu@gmail.com>
2022-08-09 11:21:15 +0800

    test for cached file reader

--- file_reader_cached.c
+++ file_reader_cached.c
@@ -24,8 +24,7 @@ typedef struct cfr_s
 } cfr_t;
 
 
-
-static uint32_t read_and_cache(cfr_t *ctx, uint8_t *out, uint32_t offset, uint32_t length)
+static uint32_t read_direct(cfr_t *ctx, uint8_t *out, uint32_t offset, uint32_t length)
 {
     uint32_t read = 0, total = 0;
     fseek(ctx->fd, offset, SEEK_SET);
@@ -45,6 +44,7 @@ static uint32_t read_and_cache(cfr_t *ctx, uint8_t *out, uint32_t offset, uint32
         {
             ctx->cache_offset = offset;
             ctx->cache_data_length = read;
+            length = length > read ? read : length;
             memcpy(out, ctx->cache, length);
             total += length;
         }
@@ -53,17 +53,17 @@ static uint32_t read_and_cache(cfr_t *ctx, uint8_t *out, uint32_t offset, uint32
 }
 
 
-/*                                                  off    len   o_c_s  o_c_l   c_s
+/*                                                  off    len   o_c_s   c_s   o_c_l
  *  cache off=4,len=3          |4|5|6|                                     
- *  read (1, 3)          |1|2|3|                     1      3            
- *  read (1, 4)          |1|2|3|4|                   1      4      3      1      0     
- *  read (4, 1)                |4|                   4      1      0      1      0 
- *  read (6, 1)                    |6|               6      1      0      1      2
- *  read (4, 3)                |4|5|6|               4      3      0      3      0
- *  read (2, 6)            |2|3|4|5|6|7|             2      6      2      3      0
- *  read (6, 1)                    |6|               6      1      0      1      2
- *  read (6, 3)                    |6|7|8|           6      3      0      1      2
- *  read (7, 3)                      |7|8|9|         7      3      
+ *  read (1, 3)          |1|2|3|                     1      3      x      x      x
+ *  read (1, 4)          |1|2|3|4|                   1      4      3      0      1
+ *  read (4, 1)                |4|                   4      1      0      0      1
+ *  read (6, 1)                    |6|               6      1      0      2      1
+ *  read (4, 3)                |4|5|6|               4      3      0      0      3
+ *  read (2, 6)            |2|3|4|5|6|7|             2      6      2      0      3
+ *  read (6, 1)                    |6|               6      1      0      2      1
+ *  read (6, 3)                    |6|7|8|           6      3      0      2      1
+ *  read (7, 3)                      |7|8|9|         7      3      x      x      x
  */
 
 static uint32_t read(file_reader_t *self, uint8_t *out, uint32_t offset, uint32_t length)
@@ -72,13 +72,10 @@ static uint32_t read(file_reader_t *self, uint8_t *out, uint32_t offset, uint32_
     uint32_t o_c_s; // out_cached_start
     uint32_t o_c_l; // out_cached_length
     uint32_t c_s;   // cached_start
-    uint32_t total = 0;
+    uint32_t total = 0, temp;
 
     if (length == 0)
         return 0;
-
-
-    VGM_PRINTF("Read from %d, len = %d\n", offset, length);
 
     // Check if reqest data is already in cache (or part of)
     if ((ctx->cache_data_length > 0) && (offset + length > ctx->cache_offset) && (offset < ctx->cache_offset + ctx->cache_data_length))
@@ -90,27 +87,38 @@ static uint32_t read(file_reader_t *self, uint8_t *out, uint32_t offset, uint32_
         else
             o_c_l = length - o_c_s;
         // transfer from catch to output
-        VGM_PRINTF("Copy cache[%d] > out[%d], len = %d\n", c_s, o_c_s, o_c_l);
         memcpy(out + o_c_s, ctx->cache + c_s, o_c_l);
         total += o_c_l;
+#ifdef CFR_MEASURE_CACHE_PERFORMACE
+        ctx->cache_hit += o_c_l;
+#endif
     }
     else
     {
-        // fetch fresh data
-        VGM_PRINTF("Fetch whole buffer\n");
-        return read_and_cache(ctx, out, offset, length);
+        // cache miss, fetch fresh data
+        temp = read_direct(ctx, out, offset, length);
+#ifdef CFR_MEASURE_CACHE_PERFORMACE
+        ctx->cache_miss += temp;
+#endif
+        return temp;
     }
     // fetch data before cached region
     if (o_c_s > 0)
     {
-        VGM_PRINTF("Fetch to out[0], len = %d\n", o_c_s);
-        total += read_and_cache(ctx, out, offset, o_c_s);
+        temp = read_direct(ctx, out, offset, o_c_s);
+#ifdef CFR_MEASURE_CACHE_PERFORMACE
+        ctx->cache_miss += temp;
+#endif
+        total += temp;
     }
     // fetch data after cached region
     if (length > o_c_s + o_c_l)
     {
-        VGM_PRINTF("Fetch to out[%d], len = %d\n", o_c_s + o_c_l, length - o_c_s - o_c_l);
-        total += read_and_cache(ctx, out + o_c_s + o_c_l, ctx->cache_offset + ctx->cache_data_length, length - o_c_s - o_c_l);
+        temp = read_direct(ctx, out + o_c_s + o_c_l, offset + o_c_s + o_c_l, length - o_c_s - o_c_l);
+#ifdef CFR_MEASURE_CACHE_PERFORMACE
+        ctx->cache_miss += temp;
+#endif
+        total += temp;        
     }
     
     return total;
@@ -159,7 +167,7 @@ file_reader_t * cfr_create(const char* fn, uint32_t cache_size)
         ctx->super.read = read;
         ctx->super.size = size;
 
-#ifdef NFR_MEASURE_CACHE_PERFORMACE
+#ifdef CFR_MEASURE_CACHE_PERFORMACE
         ctx->cache_hit = 0;
         ctx->cache_miss = 0;
 #endif
---
+++ reader_test.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <time.h>
+#include "platform.h"
+#include "file_reader_cached.h"
+#include "vgm.h"
+
+const int buf_size = 4096;
+uint8_t buf1[buf_size], buf2[buf_size];
+
+
+bool compare_buf(uint8_t *buf1, uint8_t *buf2, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (buf1[i] != buf2[i]) 
+            return false;
+    }
+    return true;
+}
+
+
+bool reader_test(file_reader_t *reader, FILE *fd, int offset, int len)
+{
+    int len1, len2;
+
+    fseek(fd, offset, SEEK_SET);
+    len1 = fread(buf1, 1, len, fd);
+    len2 = reader->read(reader, buf2, offset, len);
+    if ((len1 == len2) && compare_buf(buf1, buf2, len1))
+    {
+        VGM_PRINTF("ok\n");
+        return true;
+    }
+    else
+    {
+        VGM_PRINTF("failed\n");
+        return false;
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        VGM_PRINTERR("Usage: reader_test input\n");
+        return -1;
+    }
+   
+    FILE *fd;
+    fd = fopen(argv[1], "rb");
+    file_reader_t *reader = cfr_create(argv[1], 4096);
+
+    time_t t;
+    srand((unsigned)time(&t));
+
+    int size = reader->size(reader);
+    int offset = 0, len;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        offset += (rand() % 100);
+        len = rand() % 128 + 1;
+        VGM_PRINTF("Test\toff=%d,\tlen=%d:\t", offset, len);
+        if (!reader_test(reader, fd, offset, len)) break;
+    }
+    
+    cfr_show_cache_status(reader);
+
+    cfr_destroy(reader);
+    fclose(fd);
+
+    return 0;
+}
