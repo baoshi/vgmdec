60bbf91ae72766c9a23945c9e4d0321b00310939
baoshi <baoshi.zhu@gmail.com>
2022-08-30 09:13:38 +0800

    adding fft spectrum

--- vgmplay.c
+++ vgmplay.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <memory.h>
+#include <math.h>
 #include <SDL.h>
 #include "vgm_conf.h"
 #include "cached_file_reader.h"
 #include "vgm.h"
+#include "fft_q15.h"
 
 #define SDL_BUFFER_SIZE 2048
 #define READER_CACHE_SIZE 4096
@@ -44,11 +46,80 @@ void sdl_audio_callback(void* user, Uint8* stream, int len)
 }
 
 
+
+/*
+ * Given 44100Hz sample rate and 2048 data points, after FFT,
+ * first 1024 data corresponds to 0-22050Hz.
+ * We want first 128 points for spetrum, and we want 64 frequency bins,
+ * each bin contains 2 points
+  */
+q15_t fftdata[SDL_BUFFER_SIZE];
+#define SPECTRUM_HEIGHT 200
+#define NUM_BINS 64
+#define BIN_POINTS 2
+#define BIN_WIDTH 4
+float over_max = 0;
+void draw_spectrum(SDL_Renderer* renderer, int16_t* data, int len)
+{
+    if (len != SDL_BUFFER_SIZE)
+        return;
+    float bin_data[NUM_BINS];
+    int index;
+
+    // Obtain data
+    memcpy(fftdata, data, len * sizeof(int16_t));
+   
+    // FFT
+    fft_q15(fftdata, len);
+
+    {
+        FILE* fd = fopen("r:\\data.txt", "w");
+        for (int i = 0; i < 1024; ++i)
+        {
+            fprintf(fd, "%d\n", fftdata[i]);
+        }
+        fclose(fd);
+    }
+
+    // Collect bins
+    index = 1; // skip first point (22050/1024 = 21.5Hz)
+    for (int i = 0; i < NUM_BINS; ++i)
+    {
+        bin_data[i] = 0.0f;
+        for (int j = 0; j < BIN_POINTS; ++j)
+        {
+            bin_data[i] += fftdata[index];
+            ++index;
+        }
+    }
+    // map bin_data into 0..SPECTRUM_HEIGHT-1
+    for (int i = 0; i < NUM_BINS; ++i)
+    {
+        bin_data[i] = bin_data[i] * SPECTRUM_HEIGHT / 2000;
+    }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 192, 255);
+    for (int i = 0; i < NUM_BINS; ++i)
+    {
+        SDL_Rect rect;
+        rect.x = i * BIN_WIDTH;
+        rect.y = 210 - (int)bin_data[i];
+        rect.w = BIN_WIDTH - 1;
+        rect.h = (int)bin_data[i];
+        SDL_RenderFillRect(renderer, &rect);
+    }
+    SDL_RenderPresent(renderer);
+}
+
+
 int main(int argc, char *argv[])
 {
     file_reader_t *reader = 0;
     vgm_t *vgm = 0;
     SDL_AudioDeviceID audio_id = 0;
+    SDL_Window *screen = NULL;
+    SDL_Renderer *renderer = NULL;
     SDL_Event event;
     int quit = 0;
 
@@ -96,9 +167,16 @@ int main(int argc, char *argv[])
             break;
         }
         // Window
-        if (!SDL_CreateWindow("NSF Player", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 200, 0))
+        screen = SDL_CreateWindow("NSF Player", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 240, 240, 0);
+        if (!screen)
         {
             fprintf(stderr, "Could not set video mode: %s\n", SDL_GetError());
+            break;
+        }
+        renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_SOFTWARE);
+        if (!renderer)
+        {
+            fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
             break;
         }
         // start play
@@ -128,13 +206,16 @@ int main(int argc, char *argv[])
                 default:
                     if (event.type == buffer_ready_event_type)
                     {
-                        //int16_t* b = (int16_t*)event.user.data1;
+                        int16_t* buf = (int16_t*)event.user.data1;
                         int l = (int)(intptr_t)event.user.data2;
+
+                        printf("%d samples\n", l);
                         if (l < SDL_BUFFER_SIZE)
                         {
                             printf("Play finished.\n");
                             quit = 1;
                         }
+                        draw_spectrum(renderer, buf, l);
                     }
                     break;
                 }
@@ -142,7 +223,7 @@ int main(int argc, char *argv[])
         }
         cfreader_show_cache_status(reader);
     } while (0);
-    
+    if (screen) SDL_DestroyWindow(screen);
     if (audio_id != 0) SDL_CloseAudioDevice(audio_id);
     SDL_Quit();
     if (vgm != 0) vgm_destroy(vgm);
